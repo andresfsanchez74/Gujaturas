dd70145328c09e2e6687f906c0b1f65f78841506
andresfsanchez74 <andresfsanchez74@gmail.com>
2025-05-08 08:12:34 -0500

    prueba

--- app/src/main/java/com/example/gujaturas/Inventario.kt
+++ app/src/main/java/com/example/gujaturas/Inventario.kt
@@ -8,7 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 class Inventario : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) { //aaaa
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inventario)
 
